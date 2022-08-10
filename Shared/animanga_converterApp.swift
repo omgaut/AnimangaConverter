41c8e5766e96f702953932bd0b20187d9aed7184
Om Gautam <lionboss1010@gmail.com>
2022-08-10 12:31:47 -0400

    Initial Commit

---
+++ Shared/animanga_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  animanga_converterApp.swift
+//  Shared
+//
+//  Created by Om Gautam on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct animanga_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
